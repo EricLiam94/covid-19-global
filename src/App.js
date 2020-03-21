07b80cd82b89a23d1e24474f124cf7c66f0e756c
Eric <40311190+EricLiam94@users.noreply.github.com>
2020-03-21 21:56:34 +1100

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Maps from "./Components/Maps/Maps";
+import "./App.css";
+import Chart from "react-apexcharts";
 
 function App() {
+  var options = {
+    title: {
+      text: "check",
+      floating: true,
+      margin: 10,
+      style: { color: "white" }
+    },
+    chart: {
+      id: "basic-bar"
+    },
+    stroke: {
+      curve: "smooth"
+    },
+    xaxis: {
+      categories: ["a", "b", "c", "d", 1995, 1996, 1997, 1998, 1999],
+      rotate: -45,
+      rotateAlways: true,
+      labels: {
+        show: true,
+        align: "right",
+        minWidth: 0,
+        maxWidth: 160,
+        style: { colors: "white" }
+      }
+    },
+    fill: {
+      type: "gradient"
+    },
+    yaxis: {
+      labels: { style: { colors: "white" } }
+    },
+    legend: {
+      labels: {
+        colors: "white"
+      }
+    }
+  };
+  var series = [
+    {
+      name: "series-1",
+      data: [30, 40, 45, 50, 49, 60, 70, 91]
+    },
+    {
+      name: "series-2",
+      data: [50, 10, 25, 30, 49, 60, 70, 91]
+    },
+    {
+      name: "series-3",
+      data: [50, 10, 25, 30, 149, 61, 170, 291]
+    }
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3> Global Covid-19 </h3>
+        <Maps />
+        <Chart
+          style={{ color: "black" }}
+          options={options}
+          series={series}
+          type="area"
+          width="600px"
+        />
       </header>
     </div>
   );
---
+++ src/Components/Maps/Maps.js
@@ -0,0 +1,150 @@
+import React, { useState, useEffect } from "react";
+import ReactMapGL, { Marker } from "react-map-gl";
+import csv from "csvtojson";
+import style from "./Maps.module.css";
+
+const Maps = () => {
+  const [viewport, setViewport] = useState({
+    width: "100%",
+    height: "100%",
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 2
+  });
+  const [type, setType] = useState("Confirmed");
+  const [data, setdata] = useState([]);
+  const [display, setDisplay] = useState(null);
+  const getFormattedDate = () => {
+    var date = new Date();
+    date.setDate(date.getDate() - 2);
+    var year = date.getFullYear();
+
+    var month = (1 + date.getMonth()).toString();
+    month = month.length > 1 ? month : "0" + month;
+
+    var day = date.getDate().toString();
+    day = day.length > 1 ? day : "0" + day;
+
+    return month + "-" + day + "-" + year;
+  };
+  const url =
+    "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/" +
+    getFormattedDate() +
+    ".csv";
+  const fetchData = async () => {
+    const data = await fetch(url);
+    const res = await data.text();
+    const out = await csv().fromString(res);
+    setdata(out);
+  };
+  const onMouseOver = item => {
+    setDisplay(item);
+    console.log(item);
+  };
+
+  const onMouseOut = () => {
+    setDisplay(null);
+  };
+
+  const color = {
+    Confirmed: "#f39c12",
+    Recovered: "#2ecc71",
+    Deaths: "#c0392b"
+  };
+
+  useEffect(() => {
+    fetchData();
+    return;
+  }, []);
+
+  const APK =
+    "pk.eyJ1IjoiZXJpY2xpYW0iLCJhIjoiY2s2OTB1cTVjMGFybTNtbXJ3YjlneHhkcSJ9.m3XezVm8VJ4W-UJl4x6U7w";
+
+  const onButtonClick = (e, type) => {
+    setType(type);
+  };
+
+  return (
+    <div className={style.main}>
+      <div className={style.btns}>
+        <div
+          style={{ backgroundColor: color["Confirmed"] }}
+          onClick={e => onButtonClick(e, "Confirmed")}
+          className={type === "Confirmed" ? style.act : ""}
+        >
+          <div>Confirmed </div>
+        </div>
+        <div
+          style={{ backgroundColor: color["Recovered"] }}
+          onClick={e => onButtonClick(e, "Recovered")}
+          className={type === "Recovered" ? style.act : ""}
+        >
+          <div>Recovered</div>
+        </div>
+        <div
+          style={{ backgroundColor: color["Deaths"] }}
+          onClick={e => onButtonClick(e, "Deaths")}
+          className={type === "Deaths" ? style.act : ""}
+        >
+          <div>Deaths </div>
+        </div>
+      </div>
+      <div className={style.container}>
+        {display && (
+          <div className={style.tooltips}>
+            <div> Province/State : {display["Province/State"] || "N/A"} </div>
+            <div> Country : {display["Country/Region"]} </div>
+            <div> Confirmed : {display["Confirmed"]} </div>
+            <div> Recovered: {display["Recovered"]} </div>
+            <div> Deaths: {display["Deaths"]} </div>
+          </div>
+        )}
+        <ReactMapGL
+          {...viewport}
+          onViewportChange={setViewport}
+          mapStyle="mapbox://styles/mapbox/dark-v9"
+          mapboxApiAccessToken={APK}
+        >
+          <Markers
+            data={data}
+            type={type}
+            color={color[type]}
+            onMouseOver={onMouseOver}
+            onMouseOut={onMouseOut}
+          />
+        </ReactMapGL>
+      </div>
+    </div>
+  );
+};
+
+const Markers = ({ data, type, onMouseOver, onMouseOut, color }) => {
+  function getBaseLog(x, y) {
+    return Math.log(y) / Math.log(x);
+  }
+  return data.map(
+    (item, idx) =>
+      item[type] > 0 && (
+        <Marker
+          key={idx}
+          longitude={parseInt(item.Longitude)}
+          latitude={parseInt(item.Latitude)}
+        >
+          <div
+            className={style.circle}
+            onMouseLeave={() => onMouseOut()}
+            onMouseEnter={() => onMouseOver(item)}
+            style={{
+              width: getBaseLog(2, parseInt(item[type])),
+              height: getBaseLog(2, parseInt(item[type])),
+              backgroundColor: color
+            }}
+          >
+            {" "}
+          </div>
+        </Marker>
+      )
+  );
+};
+
+export default Maps;
